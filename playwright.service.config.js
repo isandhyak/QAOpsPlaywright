9072605e61a6edcd8871fc4c6c63bd344952cc91
isandhyak <sandhyakarande09@gmail.com>
2026-06-30 15:41:29 +0530

    Update playwright.service.config.js file changes

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -3,21 +3,27 @@ const { createAzurePlaywrightConfig, ServiceOS } = require('@azure/playwright');
 const { DefaultAzureCredential } = require('@azure/identity');
 const config = require('./playwright.config');
 
-// Combine your base config with the reporters you want to run in CI
-const customConfig = {
-  ...config,
-  reporter: [
-    ["html", { open: "never" }],
-    ["@azure/playwright/reporter"],
-  ],
-};
-
 /* Learn more about service configuration at https://aka.ms/pww/docs/config */
 export default defineConfig(
-  createAzurePlaywrightConfig(customConfig, {
+  config,
+  createAzurePlaywrightConfig(config, {
     exposeNetwork: '<loopback>',
     connectTimeout: 3 * 60 * 1000, // 3 minutes
     os: ServiceOS.LINUX,
     credential: new DefaultAzureCredential(),
-  })
+  }),
+  {
+    /*
+    Enable Playwright Workspaces Reporter:
+    Uncomment the reporter section below to upload test results and reports to Playwright Workspaces.
+
+    Note: The HTML reporter must be included before Playwright Workspaces Reporter.
+    This configuration will replace any existing reporter settings from your base config.
+    If you're already using other reporters, add them to this array.
+    */
+     reporter: [
+       ["html", { open: "never" }],
+       ["@azure/playwright/reporter"],
+     ],
+  }
 );
